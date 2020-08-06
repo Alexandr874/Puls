dbbf214f7fcd54e03e6f8bba3b4b4845857e98ef
Alexandr874 <aleks.ribinsk@gmail.com>
2020-08-06 17:40:17 +0300

    добавил скрипт  для отправления форм на сервер

--- src/js/script.js
+++ src/js/script.js
@@ -91,5 +91,22 @@ $(document).ready(function(){
 
     $('input[name=phone]').mask("+7 (999) 999-99-99");
 
+    $('form').submit(function() {
+        e.preventDefault();
+        $.ajax({
+            type: "POST",
+            url: "mailer/smart.php",
+            data: $(this).serialize()
+
+        }).done(function() {
+            $(this).find("input").val("");
+
+
+            $('form').trigger('reset');
+        });
+        return false;
+
+    });
+
    
 });
